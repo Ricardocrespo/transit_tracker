f55cfbe391c26fac3c1d4c53e77ccb8d0b90be92
Ricardo C <Ricardocrespo@gmail.com>
2025-07-13 05:47:23 -0500

    Added environment flavor

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FLAVOR=$1
+
+if [[ "$FLAVOR" != "dev" && "$FLAVOR" != "prod" ]]; then
+  echo "Usage: ./run.sh [dev|prod]"
+  exit 1
+fi
+
+echo "Running TransitTracker with flavor: $FLAVOR"
+flutter run --flavor $FLAVOR -t lib/main.dart
